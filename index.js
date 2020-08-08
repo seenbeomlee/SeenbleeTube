85bd3a37d7848448599ca8a7bd82c10f92dd9232
Seenbeom Lee <mail@seenblee.com>
2020-08-08 16:49:41 +0900

    #1.01: route

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ const handleListening = () => {
     console.log(`Listening on: http://localhost:${PORT}`)
 }
 
-app.listen(PORT, handleListening)
+function handleHome(request, response) {
+    response.send('Hello from Home')
+}
+
+function handleProfile(request, response) {
+    response.send("You are on my profile")
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
