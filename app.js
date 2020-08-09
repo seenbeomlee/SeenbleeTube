b4d6cb58bf66046ff9525abd6bb075b96b20ca3f
Seenbeom Lee <mail@seenblee.com>
2020-08-09 20:55:57 +0900

    1.07: routes
    #add global router
    #add video router
    #add user router

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import bodyParser from "body-parser";
 
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import routes from "./routes"
+
 import globalRouter from "./routers/globalRouter";
 
 const app = express();
@@ -16,8 +18,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmet());
 app.use(morgan("combined"));
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
