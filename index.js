e6b58c3eb8e72dcaf39e2cc056b6accf57cdcb33
Seenbeom Lee <mail@seenblee.com>
2020-08-09 16:33:22 +0900

    #1.04: middleWare / "morgan" & "helmet" & cookie-parser & body-parser
    1/morgan
    #app.use(morgan(tiny))와 같이 middleWare를 사용가능하다
    
    2/helmet
    #it is for se쳐갸쇼
    
    3/미들웨어가 "res.send()"명령을 사용하면, 연결을 끊는 역할을 할 수도 있다. (다음 function으로 next()를 넘겨주지 않기 때문에)

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 const PORT = 4000;
 
@@ -14,14 +19,17 @@ const handleProfile = (request, response) => {
     response.send("You are on my profile")
 }
 
-const betweenHome = (req, res, next) => {
-    console.log("Between")
-    next();
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan("combined"));
+
+const middleware = (req, res, next) => {
+    res.send('not happening');
 }
 
-app.use(betweenHome);
-
-app.get("/", handleHome);
+app.get("/", middleware, handleHome);
 
 app.get("/profile", handleProfile);
 
