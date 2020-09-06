66d58da71903e7b5522f5c53e47677980b83d6f5
Seenbeom Lee <mail@seenblee.com>
2020-09-06 14:30:33 +0900

    #2.17: practice: Template Variables in Pug
    1/ How can we give a variable to each template (it is similar with 'Flask')

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,10 +1,16 @@
 /* globalRoutes */
-export const homeController = (req, res) => res.render("Home");
-export const searchController = (req, res) => res.send("Search");
+export const homeController = (req, res) =>
+  res.render('Home', { pageTitle: 'Home' });
+export const searchController = (req, res) =>
+  res.send('Search', { pageTitle: 'Search' });
 
 /* videoRoutes */
-export const videosController = (req, res) => res.send('videos');
+export const videosController = (req, res) =>
+  res.send('videos', { pageTitle: 'Upload' });
 export const uploadController = (req, res) => res.send('upload');
-export const videoDetailController = (req, res) => res.send('videoDetail');
-export const editVideoController = (req, res) => res.send('editVideo');
-export const deleteVideoController = (req, res) => res.send('deleteVideo');
+export const videoDetailController = (req, res) =>
+  res.send('videoDetail', { pageTitle: 'Video Detail' });
+export const editVideoController = (req, res) =>
+  res.send('editVideo', { pageTitle: 'Edit Video' });
+export const deleteVideoController = (req, res) =>
+  res.send('deleteVideo', { pageTitle: 'Delete Video' });
