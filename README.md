c0974e85f1e46318c006db95ed3b887975689fa7
Seenbeom Lee <mail@seenblee.com>
2020-09-06 15:47:25 +0900

    #2.19: practice: Log In HTML
    # '|' makes all things as text (not a tag)
    1/ join.pug & login.pug ==> "include partials/socialLogin' ==> it uses socialLogin.pug as global component

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # CLONE_youtube
-cloning the YOUTUBE platform
+
+Cloning Youtube with VanillaJs and NodeJs
+
+## Pages:
+
+- [ ] Home
+- [ ] Join
+- [x] Login(Logout)
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload Video(Delete Video)
+- [ ] Video Detail
+- [ ] Edit Video
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,11 @@
 /* globalRoutes */
-export const joinController = (req, res) => res.send('join');
-export const loginController = (req, res) => res.send('login');
-export const logoutController = (req, res) => res.send('logout');
+export const joinController = (req, res) => res.render('join');
+export const loginController = (req, res) => res.render('login');
+export const logoutController = (req, res) => res.render('logout');
 
 /* userRoutes */
-export const usersController = (req, res) => res.send('users');
-export const userDetailController = (req, res) => res.send('userDetail');
-export const editProfileController = (req, res) => res.send('editProfile');
+export const usersController = (req, res) => res.render('users');
+export const userDetailController = (req, res) => res.render('userDetail');
+export const editProfileController = (req, res) => res.render('editProfile');
 export const changePasswordController = (req, res) =>
-  res.send('changePassword');
+  res.render('changePassword');
