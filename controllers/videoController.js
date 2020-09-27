88fb854378e0037f2d73af0f1ef15bc4d28fa605
Seenbeom Lee <mail@seenblee.com>
2020-09-27 16:38:24 +0900

    #3.11: Sorting video that users can see recent videos first

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -4,7 +4,7 @@ import Video from '../models/Video';
 /* globalRoutes */
 export const homeController = async (req, res) => {
   try {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({ _id: -1 });
     res.render('home', { pageTitle: 'Home', videos });
   } catch (error) {
     res.render('home', { pageTitle: 'Home', videos: [] });
