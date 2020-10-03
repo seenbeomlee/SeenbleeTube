b1178e33729f2c6039c2264e76f9aff2bc6f592c
Seenbeom Lee <mail@seenblee.com>
2020-10-03 17:15:39 +0900

    #6.3 Local Authentication with Passport
    > about user Joining
    
    #issue / when i type 'db.users.find({})' after create the user, userPassword did not shown at db.. is it a problem?
    
    1/ serializeUser
    > serialization = 어떤 정보를 쿠키에게 주느냐를 의미한다 = 지금 웹브라우저에 있는 사용자에 대해서 '쿠키'가 어떤 정보를 가질 수 있느냐
    > it tells you which field will be included in the cookie
    
    >eg > passport.serializeUser(function(user, done) {
                done(null, user.id);
              });
    
    2/ de-
    > "How do you find which user is?" = "when you get a cookie, id == 1, what will you do?
    > deserialize means how does program converts information of cookies to specific user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,27 +1,35 @@
 import routes from '../routes';
+import User from '../models/User';
 
 /* globalRoutes */
 export const getJoinController = (req, res) => {
   res.render('join', { pageTitle: 'Get Join' });
 };
 
-export const postJoinController = (req, res) => {
+export const postJoinController = async (req, res) => {
   const {
-    body: { name, eamil, password, password2 },
+    body: { name, email, password, password2 },
   } = req;
   if (password !== password2) {
     res.status(400);
     res.render('join', { pageTitle: 'Join' });
   } else {
-    // To Do: Register User
+    try {
+      const user = await User({
+        name,
+        email,
+      });
+      await User.register(user, password);
+    } catch (error) {
+      console.log(error);
+    }
     // To Do: Log user in
     res.redirect(routes.home);
   }
   res.render('join', { pageTitle: 'Join' });
 };
 
-export const getLoginController = (req, res) =>
-  res.render('login', { pageTitle: 'Login' });
+export const getLoginController = (req, res) => res.render('login', { pageTitle: 'Login' });
 export const postLoginController = (req, res) => {
   res.redirect(routes.home);
 };
