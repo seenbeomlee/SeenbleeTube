761c83ad129c6ce4f9c9e4e0efc6ec5f1643d968
Seenbeom Lee <mail@seenblee.com>
2020-10-09 22:05:20 +0900

    #8.2 Mute Unmute
    > change playBtn following by "play & pause"

--- static/main.js
+++ static/main.js
@@ -122,8 +122,10 @@ var playBtn = document.getElementById('jsPlayButton');
 function handlePlayClick() {
   if (videoPlayer.paused) {
     videoPlayer.play();
+    playBtn.innerHTML = '<i class="fas fa-pause"></i>';
   } else {
     videoPlayer.pause();
+    playBtn.innerHTML = '<i class="fas fa-play"></i>';
   }
 }
 
